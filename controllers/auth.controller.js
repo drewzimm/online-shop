c343169fc1b510d7aaf8293cb61ec6ea80cb1c47
drewzimm <drzimmerman24@gmail.com>
2022-01-28 11:29:26 -0600

    first commit with basic styling and structure

---
+++ controllers/auth.controller.js
@@ -0,0 +1,15 @@
+function getSignup(req, res) {
+  res.render('customer/auth/signup');
+}
+
+function signup(req, res) {
+
+}
+
+function getLogin(req, res) {}
+
+module.exports = {
+  getSignup: getSignup,
+  getLogin: getLogin,
+  signup: signup,
+};
