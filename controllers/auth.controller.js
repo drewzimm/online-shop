ec73d95345375f4f880f5011d5f8148d83d67654
drewzimm <drzimmerman24@gmail.com>
2022-01-29 14:48:17 -0600

    input validation on signup/login and created nav bar

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,62 @@
 const User = require('../models/user.model');
 const authUtil = require('../util/authentication');
+const validation = require('../util/validation');
+const sessionFlash = require('../util/session-flash');
+const session = require('express-session');
 
 function getSignup(req, res) {
-  res.render('customer/auth/signup');
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if (!sessionData) {
+    sessionData = {
+      email: '',
+      confirmEmail: '',
+      password: '',
+      fullname: '',
+      street: '',
+      postal: '',
+      city: '',
+    };
+  }
+
+  res.render('customer/auth/signup', { inputData: sessionData });
 }
 
 async function signup(req, res, next) {
+  const enteredData = {
+    email: req.body.email,
+    confirmEmail: req.body['confirm-email'],
+    password: req.body.password,
+    fullname: req.body.fullname,
+    street: req.body.street,
+    postal: req.body.postal,
+    city: req.body.city,
+  };
+  if (
+    !validation.userDetailsAreValid(
+      req.body.email,
+      req.body.password,
+      req.body.fullname,
+      req.body.street,
+      req.body.postal,
+      req.body.city
+    ) ||
+    !validation.emailIsConfirmed(req.body.email, req.body['confirm-email'])
+  ) {
+    sessionFlash.flashDataToSession(
+      req,
+      {
+        errorMessage:
+          'Please check your input. Password must be at least 6 characters long, postal code must be 5 characters long!',
+        ...enteredData, // spread operator. spreads out this object into the object at hand
+      },
+      function () {
+        res.redirect('/signup');
+      }
+    );
+    return;
+  }
+
   const user = new User(
     req.body.email,
     req.body.password,
@@ -16,6 +67,21 @@ async function signup(req, res, next) {
   );
 
   try {
+    const existsAlready = await user.existsAlready();
+
+    if (existsAlready) {
+      sessionFlash.flashDataToSession(
+        req,
+        {
+          errorMessage: 'User exists already! Try logging in instead!',
+          ...enteredData,
+        },
+        function () {
+          res.redirect('/signup');
+        }
+      );
+      return;
+    }
     await user.signup();
   } catch (error) {
     return next(error);
@@ -25,7 +91,15 @@ async function signup(req, res, next) {
 }
 
 function getLogin(req, res) {
-  res.render('customer/auth/login');
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if (!sessionData) {
+    sessionData = {
+      email: '',
+      password: '',
+    };
+  }
+  res.render('customer/auth/login', { inputData: sessionData });
 }
 
 async function login(req, res) {
@@ -37,15 +111,26 @@ async function login(req, res) {
     return next(error);
   }
 
+  const sessionErrorData = {
+    errorMessage:
+      'Invalid credentials. Please recheck your email and password!',
+    email: user.email,
+    password: user.password,
+  };
+
   if (!existingUser) {
-    res.redirect('/login');
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect('/login');
+    });
     return;
   }
 
   const passIsCorrect = await user.hasMatchingPassword(existingUser.password);
 
   if (!passIsCorrect) {
-    res.redirect('/login');
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect('/login');
+    });
     return;
   }
   // password and email is correct and we can log user in
