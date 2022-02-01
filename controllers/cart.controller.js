6dc510a2931a09ca8ad2a84f25b1b1686852bca7
drewzimm <drzimmerman24@gmail.com>
2022-02-01 15:29:45 -0600

    added cart view

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,7 +1,28 @@
-function addCartItem(req, res) {
-  res.locals.cart.addItem();
+const Product = require('../models/product.model');
+
+function getCart(req, res) {
+  res.render('customer/cart/cart');
+}
+
+async function addCartItem(req, res, next) {
+  let product;
+  try {
+    product = await Product.findById(req.body.productId);
+  } catch (error) {
+    next(error);
+    return;
+  }
+  const cart = res.locals.cart;
+  cart.addItem(product);
+  req.session.cart = cart; //update cart
+
+  res.status(201).json({
+    message: 'Cart updated!',
+    newTotalItems: cart.totalQuantity,
+  });
 }
 
 module.exports = {
   addCartItem: addCartItem,
-}
+  getCart: getCart,
+};
--- middlewares/cart.js
+++ middlewares/cart.js
@@ -1,12 +1,17 @@
-const Cart = require('../models/cart.model')
+const Cart = require('../models/cart.model');
 
 function initializeCart(req, res, next) {
   let cart;
-  
-  if (!req.sessions.cart) {
+
+  if (!req.session.cart) {
     cart = new Cart();
   } else {
-    cart = new Cart(req.sessions.cart.items);
+    const sessionCart = req.session.cart;
+    cart = new Cart(
+      sessionCart.items,
+      sessionCart.totalQuantity,
+      sessionCart.totalPrice
+    );
   }
 
   res.locals.cart = cart;
