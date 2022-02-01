45f6f9e3bacaeb707d09587e2d201f0ad87087c7
drewzimm <drzimmerman24@gmail.com>
2022-01-31 19:06:50 -0600

    made products editable + moved includes folder

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,14 +1,13 @@
 const Product = require('../models/product.model');
 
 async function getProducts(req, res, next) {
-  let products;
   try {
-     products = await Product.findAll();
+    const products = await Product.findAll();
+    res.render('admin/products/all-products', { products: products });
   } catch (error) {
     next(error);
     return;
   }
-  res.render('admin/products/all-products', { products: products });
 }
 
 function getNewProduct(req, res) {
@@ -31,8 +30,39 @@ async function createNewProduct(req, res, next) {
   res.redirect('/admin/products');
 }
 
+async function getUpdateProduct(req, res, next) {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.render('admin/products/update-product', { product: product });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateProduct(req, res, next) {
+  const product = new Product({
+    ...req.body,
+    _id: req.params.id,
+  });
+
+  if (req.file) {
+    product.replaceImage(req.file.filename);
+  }
+
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+  
+  res.redirect('/admin/products');
+}
+
 module.exports = {
   getProducts: getProducts,
   getNewProduct: getNewProduct,
   createNewProduct: createNewProduct,
+  getUpdateProduct: getUpdateProduct,
+  updateProduct: updateProduct,
 };
